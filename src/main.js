a5bc27a125f9250a92227133e1d3412adb9804bc
Nastya Dubrovina <anadubro@yandex.ru>
2025-09-26 18:42:06 +0300

    start project

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@
  */
 function calculateSimpleRevenue(purchase, _product) {
    // @TODO: Расчет выручки от операции
+   const { discount, sale_price, quantity } = purchase;
+
+   const transformedDiscount = discount / 100;
+   const totalBeforeDiscount = sale_price * quantity;
+   const totalAfterDiscount = totalBeforeDiscount * (1 - transformedDiscount);
+   return totalAfterDiscount;
 }
 
 /**
@@ -17,6 +23,12 @@ function calculateSimpleRevenue(purchase, _product) {
  */
 function calculateBonusByProfit(index, total, seller) {
     // @TODO: Расчет бонуса от позиции в рейтинге
+    const { profit } = seller;
+
+    if(index === 0) return profit * 0.15;
+    if(index === 1 || index === 2) return profit * 0.10;
+    if(index === total - 1) return 0;
+    return profit * 0.05;
 }
 
 /**
@@ -27,8 +39,69 @@ function calculateBonusByProfit(index, total, seller) {
  */
 function analyzeSalesData(data, options) {
     // @TODO: Проверка входных данных
+    if (!data || !data.sellers || !data.products) {
+        throw new Error('Некорректные входные данные');
+    } 
 
+    const { calculateRevenue, calculateBonus } = options;
     // @TODO: Проверка наличия опций
+
+    let sellerById = data["sellers"].reduce((acc, seller) => {
+        acc[seller.id] = seller;
+        return acc;
+    }, {});
+
+    let productBySku = data["products"].reduce((acc, product) => {
+        acc[product.sku] = product;
+        return acc;
+    }, {});
+
+    const sellerInfoById = data["sellers"].reduce((acc, seller) => {
+        acc[seller.id] = {
+            seller_id: seller.id,
+            name: `${seller.first_name} ${seller.last_name}`,
+            revenue: 0,
+            profit: 0,
+            sales_count: 0,
+            top_products: [],
+            productById: {},
+            bonus: 0,
+        };
+        return acc;
+    }, {});
+
+    for(cheque of data.purchase_records) {
+        let sellerInfo = sellerInfoById[cheque.seller_id];
+
+        for(purchaseItem of cheque.items) {
+            let itemRevenue = calculateRevenue(purchaseItem, productBySku[purchaseItem.sku]);
+            let itemProfit = itemRevenue - productBySku[purchaseItem.sku].purchase_price * purchaseItem.quantity;
+
+            sellerInfo.revenue += itemRevenue;
+            sellerInfo.profit += itemProfit;
+            sellerInfo.sales_count += purchaseItem.quantity;
+
+            if(sellerInfo.productById[purchaseItem.sku]) {
+                sellerInfo.productById[purchaseItem.sku] = 0;
+            }
+            sellerInfo.productById[purchaseItem.sku] += purchaseItem.quantity;
+        }
+    };
+
+    const sellerList = Object.values(sellerInfoById).sort(
+        (a, b) => b.profit - a.profit
+    );
+
+    sellerList.forEach((seller, index) => {
+        seller.top_products = Object.values(seller.productById).sort(
+            (a, b) => a.quantity - b.quantity
+        ).slice(0, 10);
+        delete seller.productById;
+
+        seller.bonus += calculateBonus(index, sellerList.length, seller);
+    });
+
+    return sellerList;
 
     // @TODO: Подготовка промежуточных данных для сбора статистики
 
@@ -42,3 +115,5 @@ function analyzeSalesData(data, options) {
 
     // @TODO: Подготовка итоговой коллекции с нужными полями
 }
+
+
